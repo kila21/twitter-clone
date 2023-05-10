c782af99a27f8a112bcd8e83a1819ea4288c096e
kila <luka.kilasonia.1@btu.edu.ge>
2023-05-10 23:20:41 +0400

    init form for SignUp modal

--- src/modals/sing-up/signUp.modal.tsx
+++ src/modals/sing-up/signUp.modal.tsx
@@ -1,7 +1,19 @@
+import { useForm } from 'react-hook-form'
 
 import './signUp.modal.scss'
 
 const SignUp = () => {
+    type SignUpForm = {
+        name: string,
+        password: string,
+        date: string
+    }
+
+    const { register, handleSubmit, watch, formState: {errors,isDirty, touchedFields} } = useForm<SignUpForm>()
+    const onSubmit = (data: SignUpForm) => console.log(data)
+    const password = watch('password')
+    const name = watch('name')
+
     return (
         <div className='signUp-container'>
             <div className='signUp'>
@@ -12,8 +24,25 @@ const SignUp = () => {
                 
                 <div className='signUp-create'>
                     <h1>Create your account</h1>
-                    <input type='text' placeholder='Name'/>
-                    <input type='password' placeholder='Password'/>
+                    <input 
+                    className={errors.name ? 'invalid' : ''}
+                    type='text' 
+                    placeholder='Name'
+                    {...register('name', {required: 'Required'})}
+                    />
+                    {/* {errors.name && <span className='invalid-message'>{errors.name?.message}</span>} */}
+                    {!name && touchedFields.name && <span className='invalid-message'>Required</span>}
+              
+
+                    <input 
+                    type='password' 
+                    className={errors.password ? 'invalid' : ''}
+                    placeholder='Password'
+                    {...register('password', {required: 'Required', minLength: {value: 6, message: 'Min 6 Character'}})}
+                    />
+                    {/* {errors.password && <span className='invalid-message'>{errors.password?.message}</span>} */}
+                    {!password && touchedFields.password && <span className='invalid-message'>Required</span>}
+                    {password && password.length < 6 && <span className='invalid-message'>Min 6 character</span>}
                 </div>
                     
                 <div className='signUp-date'>
@@ -21,12 +50,16 @@ const SignUp = () => {
                     <p>
                     This will not be shown publicly. Confirm your own age, even if this account is for a business, a pet, or something else.
                     </p>
-                    <input type='date'/>
+                    <input 
+                    className={errors.date ? 'invalid' : ''}
+                    type='date'
+                    {...register('date', {required: true})}
+                    />
                 </div>
             </div>
 
             <div className='confirm'>
-                <button>Confirm</button>
+                <button onClick={handleSubmit(onSubmit)}>Confirm</button>
             </div>
         
         </div>
