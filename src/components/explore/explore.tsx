3e9b9938ba631874f8acfb455d7a9a423921dee9
kila <luka.kilasonia.1@btu.edu.ge>
2023-07-05 17:03:56 +0400

    outside click to close search container

--- src/components/explore/explore.tsx
+++ src/components/explore/explore.tsx
@@ -2,7 +2,7 @@ import './explore.scss'
 import searchIcon from '../../assets/images/search.svg'
 import userIcon from '../../assets/images/user.png'
 
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { db } from '../../config/firebase'
 import { collection, getDocs, query, where } from '@firebase/firestore'
 import { useNavigate } from 'react-router-dom'
@@ -10,6 +10,8 @@ import { useNavigate } from 'react-router-dom'
 const Explore = () => {
     const [isClicked, setIsClicked] = useState(false)
     const [data, setData] = useState<any>(null)
+    
+    const inputRef = useRef<HTMLInputElement>(null);
     const navigate = useNavigate()
 
     const checkUsernameExists = async (username: string) => {
@@ -36,19 +38,40 @@ const Explore = () => {
         })
     }
 
+    const handleClick = (event: any) => {
+        if (inputRef.current && !inputRef.current.contains(event.target)) {
+            setIsClicked(false);
+        }
+    };
+
+    
+    useEffect(() => {
+        document.addEventListener('click', handleClick);
+    
+        return () => {
+          document.removeEventListener('click', handleClick);
+        };
+    }, []);
+    
+
+
     return (
         <div className='explore-container'>
-            <div onClick={() => setIsClicked(true)} className='explore-search'>
+            <div className='explore-search'>
                 <img src={searchIcon} alt='search icon'/>
-                <input onChange={(e) => getSearchUsers(e.target.value)} type='text'/>
+                <input 
+                ref={inputRef} 
+                onClick={() => setIsClicked(true)}
+                onChange={(e) => getSearchUsers(e.target.value)} 
+                type='text'/>
             </div>
 
             {isClicked && 
             <div className='explore-users-container'>
                 {!data && <div className='explore-noUser'>Try Searching For People</div>}
-                {data && data.length > 0 && data.map((user: any)=>{
+                {data && data.length > 0 && data.map((user: any, i: number)=>{
                     return (
-                    <div onClick={() => navigate(`/home/${user.username}`,{state: user.id})} className='explore-user'>
+                    <div key={user.username + i} onClick={() => navigate(`/home/${user.username}`,{state: user.id})} className='explore-user'>
                         <img src={user.photoURL || userIcon}/>
                         <h4>{user.username}</h4>
                     </div>)
